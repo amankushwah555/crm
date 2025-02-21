3caa6aa33ed2460a31d217bba76c78e9f3962146
Aman Kushwah <aman.kushwah@pinakinfosec.com>
2025-02-21 19:55:54 +0530

    Removed cors.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,9 +27,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: true, // Reflects the request origin back in the header
     credentials: true
-}));
+  }));
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 app.use("/api/auth", authRoutes)
